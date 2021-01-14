c597daa6ac72eb30b517ec386230db7c00a71def
jjwon915 <jjwon915@naver.com>
2021-01-14 14:59:40 +0900

    n과 m을 입력받아 n부터 m까지 출력

---
+++ Algorithm_LABS/n_to_m.c
@@ -0,0 +1,24 @@
+// N부터 M까지 출력하는 프로그램을 작성.
+// N과 M을 공백을 두고 입력받아 N부터 M까지 출력하는 프로그램.
+
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    int cnt = 0;
+    scanf("%d %d", &N, &M);
+
+    for (N; N <= M; N++) {
+        if (cnt < 7) {
+            printf("%d ", N);
+            cnt++;
+        }
+        else if (cnt == 7) {
+            printf("%d\n", N);
+            cnt = 0;
+        }
+    }
+
+
+    return 0;
+}
