9a638fa956544c0265ef6ce42878817652389426
jjwon915 <jjwon915@naver.com>
2021-01-14 13:13:05 +0900

    test

---
+++ number_count.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int result;
+    int array[10] = { 0, };
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    int next = a * b * c;
+
+    while (next > 0) {
+        result = next % 10;
+        next = next / 10;
+        array[result]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", array[i]);
+    }
+
+    return 0;
+}
