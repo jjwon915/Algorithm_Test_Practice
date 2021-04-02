7accf4a7b03b8e9359dfa1076de1022f9695a47b
jjwon915 <jjwon915@naver.com>
2021-04-02 16:46:22 +0900

    숫자의 총 개수(small)

---
+++ Algorithm_Test/sumOfNumbers_small.cpp
@@ -0,0 +1,24 @@
+﻿// 숫자의 총 개수(small)
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, i, cnt = 0, tmp;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		tmp = i;
+		while (tmp > 0) {
+			tmp = tmp / 10;
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
