6508065a5c6d2cc978ef027ec7bc7944469c8a67
jjwon915 <jjwon915@naver.com>
2021-04-02 18:37:18 +0900

    소수의 개수 구하기 - 200000개의 경우 중첩 for문을 통해 계산하면 1초안에 계산이 불가능하기 때문에  j를 제곱한 것이 i보다 작은지 판별해 시간을 단축시킨다.

---
+++ Algorithm_Test/sumOfDigitNumbers.cpp
@@ -0,0 +1,28 @@
+﻿// 소수의 개수
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, i, j, flag, cnt = 0;
+	scanf("%d", &n);
+
+	for (int i = 2; i <= n; i++) {
+		flag = 1;
+		for (j = 2; j*j <= i; j++) {
+			if (i % j == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1) {
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
