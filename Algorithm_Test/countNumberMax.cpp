c535e102d62dd9b9ab14527c4907365b53737a4a
jjwon915 <jjwon915@naver.com>
2021-04-02 18:35:55 +0900

    가장 많이 사용된 숫자.

---
+++ Algorithm_Test/countNumberMax.cpp
@@ -0,0 +1,33 @@
+﻿// 가장 많이 사용된 자릿수
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int cnt[10];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int digit, max = -2147000000, res;
+	char a[101];
+	scanf("%s", &a);
+
+	for(int i = 0; a[i] != '\0'; i++){
+		digit = a[i] - 48;
+		cnt[digit]++;
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		if (cnt[i] > max) {
+			max = cnt[i];
+			res = i;
+		}
+		
+		if (cnt[i] == max && i > res) {
+			res = i;
+		}
+	}
+
+	printf("%d", res);
+
+	return 0;
+}
