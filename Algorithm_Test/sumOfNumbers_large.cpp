1545708635f48b948de25d52a096349ff0dcb442
jjwon915 <jjwon915@naver.com>
2021-04-02 16:46:45 +0900

    숫자의 총 개수(large)

---
+++ Algorithm_Test/sumOfNumbers_large.cpp
@@ -0,0 +1,24 @@
+﻿// 숫자의 총 개수(large)
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int n, sum = 0, cnt = 1, d = 9, res = 0;
+
+	scanf("%d", &n);
+
+	while (sum + d < n) {
+		res = res + (cnt * d);
+		sum = sum + d;
+		cnt++;
+		d = d * 10;
+	}
+
+	res = res + ((n - sum) * cnt);
+	printf("%d", res);
+
+	return 0;
+}
