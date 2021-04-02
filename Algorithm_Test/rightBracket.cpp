57fe440181f4c71a01c55208543aa7789d713fea
jjwon915 <jjwon915@naver.com>
2021-04-02 16:44:58 +0900

    올바른 괄호인지 파악

---
+++ Algorithm_Test/rightBracket.cpp
@@ -0,0 +1,27 @@
+﻿// 올바른 괄호
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	char a[100];
+	int i, cnt = 0;
+	scanf("%s", &a);
+
+	for (i = 0; a[i] != '\0'; i++) {
+		if (a[i] == '(')
+			cnt++;
+		else if (a[i] == ')')
+			cnt--;
+		if (cnt < 0)
+			break;
+	}
+
+	if (cnt == 0)
+		printf("YES\n");
+	else
+		printf("NO\n");
+	
+	return 0;
+}
