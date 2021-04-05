05d2be36555d53e75f3a97b2bf6f64d1c5398a12
jjwon915 <jjwon915@naver.com>
2021-04-05 23:59:14 +0900

    층간소음 최대 시간 구하기.

---
+++ Algorithm_Test/floorNoise.cpp
@@ -0,0 +1,32 @@
+﻿// 층간 소음
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, m, noise;
+	scanf("%d %d", &n, &m);
+	
+	int large = -1, count = 0;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &noise);
+		if (noise > m) {
+			count++;
+		}
+		else {
+			if (large < count)
+				large = count;
+			count = 0;
+		}
+		if (i == n - 1 && large < count) {
+			large = count;
+		}
+	}
+
+	printf("%d\n", large);
+
+	return 0;
+}
