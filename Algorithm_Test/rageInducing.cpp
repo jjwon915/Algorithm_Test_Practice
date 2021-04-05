16c1164269f158b28940707c22f36db1440fac5d
jjwon915 <jjwon915@naver.com>
2021-04-06 00:15:38 +0900

    분노 유발자 찾기

---
+++ Algorithm_Test/rageInducing.cpp
@@ -0,0 +1,30 @@
+﻿// 분노 유발자
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n;
+	scanf("%d", &n);
+
+	int a[100];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	
+	int max = a[n - 1];
+	int count = 0;
+
+	for (int i = n-2; i > -1; i--) {
+		if (a[i] > max) {
+			max = a[i];
+			count++;
+		}
+	}
+
+	printf("%d", count);
+
+	return 0;
+}
