c462e64ee6a9f917fa00ffc48ddb3ab8220fc527
jjwon915 <jjwon915@naver.com>
2021-04-05 23:37:16 +0900

    선생님 퀴즈

---
+++ Algorithm_Test/teacherQuiz.cpp
@@ -0,0 +1,24 @@
+﻿// 선생님 퀴즈
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, a, b, sum = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		sum = ((a + 1) * a) / 2;
+		if (sum == b) {
+			printf("YES\n");
+		}
+		else {
+			printf("NO\n");
+		}
+	}
+
+	return 0;
+}
