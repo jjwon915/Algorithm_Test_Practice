843a5c8ec0f7027da3f6d3ba4ff5627bcbcace62
jjwon915 <jjwon915@naver.com>
2021-04-07 13:00:50 +0900

    연속부분 증가수열

---
+++ Algorithm_Test/consecutiveNumberSequence.cpp
@@ -0,0 +1,36 @@
+﻿// 연속 부분 증가수열
+#include <stdio.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, pre, now, cnt = 0, max;
+	int large = -217000000;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &now);
+		if (i == 0) {
+			cnt = 1;
+			max = cnt;
+			pre = now;
+		}
+		else {
+			if (now >= pre) {
+				cnt++;
+				pre = now;
+				if (cnt > max) max = cnt;
+			}
+			else {
+				cnt = 1;
+				pre = now;
+			}
+		}
+	}
+
+	printf("%d", max);
+
+	return 0;
+}
