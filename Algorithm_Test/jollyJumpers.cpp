ef28997ac5fb6c2a2fbf899c146c986ed6d5cb47
jjwon915 <jjwon915@naver.com>
2021-04-07 14:57:21 +0900

    jolly Jumpers

---
+++ Algorithm_Test/jollyJumpers.cpp
@@ -0,0 +1,39 @@
+﻿// Jolly Jumpers
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int a[100];
+int minus[100];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n;
+	scanf("%d", &n);
+
+	int res, sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		res = abs(a[i] - a[i + 1]);
+		if(res < n)
+			minus[res]++;
+	}
+
+	for (int i = 1; i < n; i++) {
+		if (minus[i] != 0)
+			sum++;
+	}
+	
+	if (sum == n - 1)
+		printf("YES");
+	else
+		printf("NO");
+
+	return 0;
+}
