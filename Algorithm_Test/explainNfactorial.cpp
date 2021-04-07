a7b48102f6e90007ee9c04adb96a4b69d0a878ae
jjwon915 <jjwon915@naver.com>
2021-04-07 15:28:07 +0900

    N!의 표현법(소인수 분해 응용)

---
+++ Algorithm_Test/explainNfactorial.cpp
@@ -0,0 +1,38 @@
+﻿// N!의 표현법
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, cnt, tmp, j;
+	scanf("%d", &n);
+
+	vector<int> ch(n + 1);
+
+	for (int i = 2; i <= n; i++) {
+		tmp = i; 
+		j = 2; 
+		while (1) {
+			if (tmp % j == 0) {
+				tmp = tmp / j;
+				ch[j]++;
+			}
+			else
+				j++;
+			if (tmp == 1)
+				break;
+		}
+	}
+
+	printf("%d! = ", n);
+	for (int i = 2; i <= n; i++) {
+		if (ch[i] != 0)
+			printf("%d ", ch[i]);
+	}
+
+	return 0;
+}
