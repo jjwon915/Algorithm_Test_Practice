224cd88eccf2e3b5e5b67f6fc3c707ca62e343fe
jjwon915 <jjwon915@naver.com>
2021-04-07 14:57:12 +0900

    석차 구하기

---
+++ Algorithm_Test/findOrder.cpp
@@ -0,0 +1,35 @@
+﻿// 석차구하기
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#pragma warning(disable: 4996)
+#include <vector>
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n;
+	scanf("%d", &n);
+
+	vector<int> a(n);
+	vector<int> b(n, 1);
+	
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (a[i] < a[j]) {
+				b[i]++;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", b[i]);
+	}
+
+
+	return 0;
+}
