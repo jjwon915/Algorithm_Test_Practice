a3c08fa3b392188a69a3d1f416770b3f271c838c
jjwon915 <jjwon915@naver.com>
2021-04-07 15:10:35 +0900

    마라톤(한국정보올림피아드)

---
+++ Algorithm_Test/marathon.cpp
@@ -0,0 +1,34 @@
+﻿// 마라톤
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int a[100];
+int minus[100];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, cnt;
+	scanf("%d", &n);
+
+	vector<int> a(n + 1);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	printf("1 ");
+
+	for (int i = 2; i <= n; i++) {
+		cnt = 0;
+		for (int j = i - 1; j >= 1; j--) {
+			if (a[j] >= a[i])
+				cnt++;
+		}
+		printf("%d ", cnt + 1);
+	}
+
+	return 0;
+}
