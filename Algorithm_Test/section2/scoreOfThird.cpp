db874cf096f9726921dd2ff01dce9e7ed56a9a21
jjwon915 <jjwon915@naver.com>
2021-04-08 16:01:58 +0900

    3등의 성적 구하기(선택정렬 사용)

---
+++ Algorithm_Test/section2/scoreOfThird.cpp
@@ -0,0 +1,37 @@
+﻿// 3등의 성적은?(선택정렬)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int a[100], n, index, tmp;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n-1; i++) {
+		index = i;
+		for (int j = index; j < n; j++) {
+			if (a[index] < a[j]) index = j;
+		}
+		tmp = a[index];
+		a[index] = a[i];
+		a[i] = tmp;
+	}
+
+	int res = 1;
+	for (int i = 1; i < n; i++) {
+		if (a[i] < a[i - 1])res++;
+		if (res == 3) {
+			printf("%d", a[i]);
+			break;
+		}
+	}
+	return 0;
+}
