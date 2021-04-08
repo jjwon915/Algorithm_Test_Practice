d60dd64def32ea41b6992a70b8e401a63ebf67d7
jjwon915 <jjwon915@naver.com>
2021-04-08 15:34:15 +0900

    3의 개수 구하기

---
+++ Algorithm_Test/section1/numberOfThree(large).cpp
@@ -0,0 +1,32 @@
+﻿// 3의 개수는?(large) : 입력 1000000000까지 가능.
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n;
+	int lt = 1, cur, rt, k = 1, res = 0;
+	scanf("%d", &n);
+
+	while (lt != 0) {
+		lt = n / (k * 10);
+		rt = n % k;
+		cur = (n / k) % 10;
+		if (3 < cur) {
+			res = res + ((lt + 1) * k);
+		}
+		else if (3 == cur) {
+			res = res + ((lt * k) + (rt + 1));
+		}
+		else res = res + (lt * k);
+		k = k * 10;
+	}
+
+	printf("%d", res);
+
+	return 0;
+}
---
+++ Algorithm_Test/section1/numberOfThree(small).cpp
@@ -0,0 +1,28 @@
+﻿// 3의 개수는?(small)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, cnt = 0, digit, tmp;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		tmp = i;
+		while (tmp > 0) {
+			digit = tmp % 10;
+			if (digit == 3) {
+				cnt++;
+			}
+			tmp = tmp / 10;
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
