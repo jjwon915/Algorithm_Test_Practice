0e9a4a2eb2a243a2e3f2ce9a7b3fbdb797ff877e
jjwon915 <jjwon915@naver.com>
2021-04-08 16:37:14 +0900

    Special Sort(버블정렬 응용 : 구글 인터뷰 문제) - 음의 정수는 항상 양의 정수 앞쪽. 순서는 변함 없이.

---
+++ Algorithm_Test/section2/specialSort.cpp
@@ -0,0 +1,35 @@
+﻿// Special Sort(버블정렬 응용:구글 인터뷰)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int a[100], ch = 0, tmp;
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (a[j] > 0 && a[j + 1] < 0) {
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+
+	return 0;
+}
