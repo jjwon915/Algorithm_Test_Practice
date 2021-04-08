5e72ffca48ae242302073114e98a47976fc5e04e
jjwon915 <jjwon915@naver.com>
2021-04-08 16:01:43 +0900

    버블정렬

---
+++ Algorithm_Test/section2/bubbleSort.cpp
@@ -0,0 +1,32 @@
+﻿// 버블정렬
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int a[100], tmp, n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
