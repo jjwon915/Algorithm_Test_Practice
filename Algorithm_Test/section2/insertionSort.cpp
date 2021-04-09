b1522709e2b3d4f62769bddc4cb76e656a9cc0aa
jjwon915 <jjwon915@naver.com>
2021-04-09 11:17:04 +0900

    삽입정렬

---
+++ Algorithm_Test/section2/insertionSort.cpp
@@ -0,0 +1,35 @@
+﻿// 삽입정렬
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+
+	int n, a[100];
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	int tmp, i, j;
+	
+	for (i = 0; i < n; i++) {
+		tmp = a[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (a[j] > tmp) {
+				a[j + 1] = a[j];
+			}
+			else break;
+		}
+		a[j + 1] = tmp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
