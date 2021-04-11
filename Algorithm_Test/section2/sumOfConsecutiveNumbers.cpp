9bdc356660b094da9896884f7595ef90f33b22dd
jjwon915 <jjwon915@naver.com>
2021-04-12 00:00:39 +0900

    연속된 자연수의 합으로 나타내기.

---
+++ Algorithm_Test/section2/sumOfConsecutiveNumbers.cpp
@@ -0,0 +1,31 @@
+﻿// 연속된 자연수의 합
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	
+	int n, a = 1, cnt = 0, tmp, i;
+	scanf("%d", &n);
+
+	tmp = n;
+	n--;
+	while (n > 0) {
+		a++;
+		n = n - a;
+
+		if (n % a == 0) {
+			for (i = 1; i < a; i++) {
+				printf("%d + ", (n / a) + i);
+			}
+			printf("%d = %d\n", (n / a) + i, tmp);
+			cnt++;
+		}
+	}
+	printf("%d\n", cnt);
+	
+	return 0;
+}
