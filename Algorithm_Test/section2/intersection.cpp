a64d6fa17b172d0ec03bf2bf0195a86159da06ec
jjwon915 <jjwon915@naver.com>
2021-04-11 23:36:02 +0900

    교집합(투포인터알고리즘) - MS 인터뷰, 제한시간 1초

---
+++ Algorithm_Test/section2/intersection.cpp
@@ -0,0 +1,45 @@
+﻿// 교집합(투포인터 알고리즘)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, m, p1 = 0, p2 = 0, p3 =0;
+	
+	scanf("%d", &n);
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	// sort 함수는 algorithm header파일에서 제공하고 오름차순 정렬된다.
+	sort(a.begin(), a.end());
+
+	scanf("%d", &m);
+	vector<int> b(m), c(m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &b[i]);
+	}
+	sort(b.begin(), b.end());
+
+	while (p1 < n && p2 < m) {
+		if (a[p1] == b[p2]) {
+			c[p3++] = a[p1++];
+			p2++;
+		}
+		else if (a[p1] < b[p2]) {
+			p1++;
+		}
+		else p2++;
+	}
+	
+	for (int i = 0; i < p3; i++) {
+		printf("%d ", c[i]);
+	}
+
+	
+	return 0;
+}
