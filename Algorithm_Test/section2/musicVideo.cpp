1c571b7b3422bf4f3092afd6d2ac0abd09d97bad
jjwon915 <jjwon915@naver.com>
2021-04-13 13:25:37 +0900

    뮤직비디오(이분검색 응용 : 결정알고리즘)

---
+++ Algorithm_Test/section2/musicVideo.cpp
@@ -0,0 +1,50 @@
+﻿// 뮤직비디오(이분검색 응용 : 결정 알고리즘)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int a[1001], n;
+
+int Count(int size) {
+	int i, cnt = 1, sum = 0;
+	for (i = 1; i <= n; i++) {
+		if (sum + a[i] > size) {
+			sum = a[i];
+			cnt++;
+		}
+		else {
+			sum += a[i];
+		}
+	}
+	return cnt;
+}
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int m, lt = 1, rt = 0, mid, res, max = -2147000000;
+	scanf("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		rt += a[i];
+		if (a[i] > max) max = a[i];
+	}
+
+	while (lt <= rt) {
+		mid = (lt + rt) / 2;
+		if (mid >= max && Count(mid) <= m) {
+			res = mid;
+			rt = mid - 1;
+		}
+		else {
+			lt = mid + 1;
+		}
+	}
+
+	printf("%d\n", res);
+
+	return 0;
+}
