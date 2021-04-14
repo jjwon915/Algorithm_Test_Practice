0aba8027edba2f1a3a3d39cfff8c68d0804e9766
jjwon915 <jjwon915@naver.com>
2021-04-14 22:22:44 +0900

    마구간 정하기(이분검색 응용)

---
+++ Algorithm_Test/section2/selectHorse.cpp
@@ -0,0 +1,48 @@
+﻿// 마구간 정하기(이분검색 응용)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int n;
+int Count(int len, int a[]) {
+	int i, cnt = 1, pos = a[1];
+	for (i = 2; i <= n; i++) {
+		if (a[i] - pos >= len) {
+			cnt++;
+			pos = a[i];
+		}
+	}
+	return cnt;
+}
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int c;
+	scanf("%d %d", &n, &c);
+
+	int* a = new int[n + 1];
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	sort(a + 1, a + n + 1);
+	int rt = a[n], lt = 1, mid, res;
+
+	while (lt <= rt) {
+		mid = (lt + rt) / 2;
+		if (Count(mid, a) >= c) {
+			res = mid;
+			lt = mid + 1;
+		}
+		else rt = mid - 1;
+	}
+
+	printf("%d", res);
+	delete[] a;
+	
+	return 0;
+}
