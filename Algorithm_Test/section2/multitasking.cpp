ee01218cd177b19767401a86dd6d358cc2667ee7
jjwon915 <jjwon915@naver.com>
2021-04-30 23:01:01 +0900

    멀티태스킹(카카오 먹방 문제 변형)

---
+++ Algorithm_Test/section2/multitasking.cpp
@@ -0,0 +1,45 @@
+﻿// 멀티태스킹(카카오 먹방문제 변형)
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#pragma warning(disable: 4996)
+
+int a[2001];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n, p = 0, cnt = 0, tot = 0;
+	scanf("%d", &n);
+	
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		tot += a[i];
+	}
+
+	int k;
+	scanf("%d", &k);
+	if (k >= tot) {
+		printf("-1\n");
+		return 0;
+	}
+
+	while (1) {
+		p++;
+		if (p > n) p = 1;
+		if (a[p] == 0) continue;
+		a[p]--;
+		cnt++;
+		if (cnt == k) break;
+	}
+
+	while (1) {
+		p++;
+		if (p > n) p = 1;
+		if (a[p] != 0) break;
+	}
+
+	printf("%d\n", p);
+	
+	return 0;
+}
