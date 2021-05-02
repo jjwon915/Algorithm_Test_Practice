26b3c033ecfadd98d6baba6d9f414d22633111e5
jjwon915 <jjwon915@naver.com>
2021-05-02 23:51:55 +0900

    [기초-입출력] 연월일 입력받아 그대로 출력하기

---
+++ CodeUp100/1019_18732299(AC).cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(){
+    int year;
+    int month;
+    int day;
+    
+    scanf("%d.%d.%d", &year, &month, &day);
+    printf("%0004d.%02d.%02d", year, month, day);
+    
+    return 0;
+}
