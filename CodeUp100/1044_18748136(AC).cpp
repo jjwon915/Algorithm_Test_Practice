38bf2596b16edcdf9a61b7dd90a248529f78d8e3
jjwon915 <jjwon915@naver.com>
2021-05-03 18:10:03 +0900

    기초

---
+++ CodeUp100/1044_18748136(AC).cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(){
+    long int a;
+    scanf("%ld", &a);
+    
+    printf("%ld", ++a);
+    
+    return 0;
+}
---
+++ CodeUp100/1045_18748364(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(){
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+    printf("%lld\n", a+b);
+    printf("%d\n", a-b);
+    printf("%lld\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+    printf("%.2f", (float)a/b);
+    
+    return 0;
+}
+
+
