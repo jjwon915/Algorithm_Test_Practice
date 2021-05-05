26388dcef102cc6b18c694a80a4f0b8dbdff233e
jjwon915 <jjwon915@naver.com>
2021-05-05 22:55:48 +0900

    [기초-종합]

---
+++ CodeUp100/1079_18790916(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(){
+    char c;
+    while(1){
+        scanf("%c", &c);
+        if(c == 'q'){
+            printf("%c", c);
+            break;
+        }
+        else
+            printf("%c", c);
+    }
+    
+    return 0;
+}
---
+++ CodeUp100/1080_18791123(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(){
+    int num,sum = 0;
+    scanf("%d",&num);
+    int i = 0;
+    
+    while(1){
+        i++;
+        sum+=i;
+        if(sum>=num){
+            break;
+        }
+    }
+    printf("%d",i);
+    return 0;
+}
