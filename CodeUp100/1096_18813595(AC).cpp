e9c7255e0b1c781155790f6a6f4a00e25950652b
jjwon915 <jjwon915@naver.com>
2021-05-06 17:32:22 +0900

    [기초-2차원배열]

---
+++ CodeUp100/1096_18813595(AC).cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main() {
+    int A[20][20] = {0,};
+    int n, x, y;
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++){
+        scanf("%d %d", &x, &y);
+        A[x][y] = 1;
+    }
+    
+    for (int i = 1; i <= 19; i++){
+        for (int j = 1; j <= 19; j++){
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
