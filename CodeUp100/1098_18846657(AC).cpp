6b7c7498894f6dc01c5a0ce271de656a510b4c00
jjwon915 <jjwon915@naver.com>
2021-05-08 00:12:50 +0900

    [기초-2차원배열] 설탕과자 뽑기

---
+++ CodeUp100/1098_18846657(AC).cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int main(){
+    int a[1000][1000] = {0,};
+    int w, h;
+    scanf("%d %d", &w, &h);
+    int n;
+    scanf("%d", &n);
+    
+    int l, d, x, y;
+    for(int i = 0; i < n; i++){
+        scanf("%d %d %d %d", &l, &d, &x, &y);
+        if(d == 0){
+            for(int j = 0; j < l; j++){
+                a[x][y] = 1;
+                y += 1;
+            }
+        }
+        else if(d == 1){
+            for(int j = 0; j < l; j++){
+                a[x][y] = 1;
+                x += 1;
+            }
+        }
+    }
+    
+    for(int i = 1; i <= w; i++){
+        for(int j = 1; j <= h; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
