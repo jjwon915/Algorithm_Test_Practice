8b1b1685e4450439dd44d890b0f00e45089b9ebd
jjwon915 <jjwon915@naver.com>
2021-05-12 21:32:27 +0900

    그룹 단어 체커 - 백준 1316번 - 입력받은 String을 char 배열로 변환해 그룹단어인지 판단한다. - 한 단어에 대한 검사가 끝나면 체크하기 위한 count 배열을 모두 0으로 초기화한다.

---
+++ Practice/src/baekjoon/CheckGroupWords.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CheckGroupWords {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] count = new int[26];
+		int c = 0, result = 0;
+		
+		for(int i = 0; i < n; i++) {
+			String s = in.next();
+			char[] a = s.toCharArray();
+			for(int j = 0; j < a.length; j++) {
+				if(a.length == 1) {
+					result++;
+					break;
+				}
+				c = (int)a[j] - 97;
+
+				if(j == 0) count[c]++;
+				else if(a[j] != a[j-1] && count[c] > 0) {
+					Arrays.fill(count, 0);
+					break;
+				}
+				else{
+					if(j == a.length - 1) {
+						result++;
+						Arrays.fill(count, 0);
+					}
+					count[c]++;
+				}
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
