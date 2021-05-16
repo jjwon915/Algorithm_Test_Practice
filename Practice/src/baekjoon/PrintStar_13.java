d423a9a583bd055af42d0b40802b818e6b2fb8d2
jjwon915 <jjwon915@naver.com>
2021-05-16 22:25:27 +0900

    별찍기 - 13 (백준 2523번)

---
+++ Practice/src/baekjoon/PrintStar_13.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class PrintStar_13 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i = 1; i <= n ; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j < n - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
