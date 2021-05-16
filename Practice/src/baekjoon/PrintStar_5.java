575b5822a908fc5776d9d3b3cf74f69588288f73
jjwon915 <jjwon915@naver.com>
2021-05-16 22:48:48 +0900

    별찍기 - 5 - 백준 2442번 - 공백은 n - i개 찍히고, 별은 각 줄마다 1개부터 홀수개로 찍힌다.

---
+++ Practice/src/baekjoon/PrintStar_5.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class PrintStar_5 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < i * 2 + 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
