458718e240254d6c4c21020b541d0d0a35c92bf3
jjwon915 <jjwon915@naver.com>
2021-05-20 23:12:07 +0900

    마이크로소프트 로고 - 백준 5338번

---
+++ Practice/src/baekjoon/MicrosoftLogo.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+public class MicrosoftLogo {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+
+	}
+
+}
