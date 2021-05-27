cbb80314838c3b407a9ff4cdf056a179a07108cb
jjwon915 <jjwon915@naver.com>
2021-05-27 14:53:31 +0900

    이상한 곱셈 - update

--- Practice/src/baekjoon_String/WeirdMultiplication.java
+++ Practice/src/baekjoon_String/WeirdMultiplication.java
@@ -3,8 +3,6 @@ package baekjoon_String;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class WeirdMultiplication {
 
