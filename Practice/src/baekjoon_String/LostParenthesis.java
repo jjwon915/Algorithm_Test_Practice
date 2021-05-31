0f862c705dc21f93a0d71b67481c7df4b51af1c3
jjwon915 <jjwon915@naver.com>
2021-05-31 14:13:43 +0900

    잃어버린 괄호 - 백준 1541번 - 괄호가 빠진 +, - 를 활용한 식이 주어지면 가장 작은 수를 결과로 나올 수 있게끔 만드는 코드를 구현한다. - 가장 최소의 수를 만들기 위해서는 +연산을 먼저 수행하고, -연산을 수행해야 가장 작은 수를 만들 수 있기 때문에 먼저 -부호로 입력받은 식을 분리한다. - 그 다음 분리한 각각의 자리를 다시 +부호로 분리하여 +연산을 먼저 수행한다. - sum을 Integer.MAX_VALUE로 초기화해서 가장 처음 더한 값이 sum에 저장될 수 있도록 하고, 그 다음 값들은 -연산을 수행한다.

---
+++ Practice/src/baekjoon_String/LostParenthesis.java
@@ -0,0 +1,31 @@
+package baekjoon_String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LostParenthesis {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] sub = br.readLine().split("-");
+		int sum = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < sub.length; i++) {
+			int tmp = 0;
+			
+			String[] add = sub[i].split("\\+");
+			for(int j = 0; j < add.length; j++) {
+				tmp += Integer.parseInt(add[j]);
+			}
+			
+			if(sum == Integer.MAX_VALUE) {
+				sum = tmp;
+			}
+			else {
+				sum -= tmp;
+			}
+		}	
+		System.out.println(sum);
+	}
+}
