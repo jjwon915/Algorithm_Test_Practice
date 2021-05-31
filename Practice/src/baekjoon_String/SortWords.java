aa47079e0959fd634a8223eb9a793bb56c2569ae
jjwon915 <jjwon915@naver.com>
2021-05-31 13:38:22 +0900

    단어 정렬 - 백준 1181번 - 알파벳 소문자로 이루어진 N개의 단어가 입력되면 길이가 짧은것, 길이가 같으면 사전 순으로 정렬해 출력한다. - 중복되는 문자열은 한 번만 출력한다. - Arrays.sort()에서 Comparator 클래스를 이용해 정렬을 하였다. 문자열의 길이가 같을 때는 compareTo()메소드를 이용해 사전순으로 정렬하였고, 문자열의 길이가 같지 않으면 길이를 비교하였다.

---
+++ Practice/src/baekjoon_String/SortWords.java
@@ -0,0 +1,41 @@
+package baekjoon_String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.ListIterator;
+
+public class SortWords {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+
+		System.out.println(arr[0]);
+		for(int i = 1; i < arr.length; i++) {
+			if(!arr[i].equals(arr[i-1]))
+				System.out.println(arr[i]);
+		}
+	}
+}
