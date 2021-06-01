f237ecf221e44fdf724ad1e7253423c104ae8b7c
jjwon915 <jjwon915@naver.com>
2021-06-01 15:11:11 +0900

    공통 부분 문자열

--- Practice/src/baekjoon_String/SamePartOfString.java
+++ Practice/src/baekjoon_String/SamePartOfString.java
@@ -1,9 +1,31 @@
 package baekjoon_String;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SamePartOfString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String one = br.readLine();
+		String two = br.readLine();
+		
+		int len1 = one.length();
+		int len2 = two.length();
+		int count = 0;
+		int [][] map = new int[len1 + 1][len2 + 1];
+		
+		for(int i = 1; i <= len1; i++) {
+			for(int j = 1; j <= len2; j++) {
+				if(one.charAt(i-1) == two.charAt(j-1)) {
+					map[i][j] = map[i-1][j-1] + 1;
+					count = Math.max(count, map[i][j]);
+				}
+			}
+		}
+		
+		System.out.println(count);
 
 	}
 
