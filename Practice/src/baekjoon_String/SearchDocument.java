630028b458c3bed34659b44a1d488bdae1ea25dd
jjwon915 <jjwon915@naver.com>
2021-06-01 16:06:40 +0900

    문서 검색

--- Practice/src/baekjoon_String/SearchDocument.java
+++ Practice/src/baekjoon_String/SearchDocument.java
@@ -1,10 +1,29 @@
 package baekjoon_String;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SearchDocument {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String search = br.readLine();
+		
+		int L = str.length();
+		int sL = search.length();
+		int count = 0;
+		
+		for(int i = 0; i <= L - sL; i++) {
+			if(str.substring(i, i + sL).equals(search)) {
+				i = i + sL - 1;
+				count++;
+			}
+		}
 
+		System.out.println(count);
 	}
 
 }
