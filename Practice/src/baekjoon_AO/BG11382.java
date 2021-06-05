dc5f680bbe481dac03c7be9275e4d54b098c1691
jjwon915 <jjwon915@naver.com>
2021-06-05 23:17:48 +0900

    꼬마 정민 - 백준 11832번 - 정수 a, b, c를 입력받아 합을 출력한다. 세 정수는 10의 12승까지 입력가능하므로 long형으로 읽는다.

---
+++ Practice/src/baekjoon_AO/BG11382.java
@@ -0,0 +1,19 @@
+package baekjoon_AO;
+
+import java.util.Scanner;
+
+public class BG11382 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long a = in.nextLong();
+		long b = in.nextLong();
+		long c = in.nextLong();
+		long sum = a + b + c;
+		
+		System.out.println(sum);
+		
+
+	}
+
+}
