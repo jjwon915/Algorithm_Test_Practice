76a22dcabec804f980555eb0f95c4bd0e00d048f
jjwon915 <jjwon915@naver.com>
2021-06-06 23:41:26 +0900

    홍익대학교 - 백준 16394번 - 홍익대학교가 개교 몇주년인지 출력하기.

---
+++ Practice/src/baekjoon_AO/Hongik.java
@@ -0,0 +1,13 @@
+package baekjoon_AO;
+
+import java.util.Scanner;
+
+public class Hongik {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n - 1946);
+	}
+
+}
