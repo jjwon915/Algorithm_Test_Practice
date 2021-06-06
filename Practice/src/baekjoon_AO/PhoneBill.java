ccd6fa0ca3f1704d98d3229a8772bedb45dba86c
jjwon915 <jjwon915@naver.com>
2021-06-07 01:48:45 +0900

    핸드폰 요금 - 백준 1267번 - 30초마다 10원씩 청구되는 요금제와 60초마다 15원씩 청구되는 요금제의 금액을 비교해 저렴한 요금제와 요금을 출력한다.

--- Practice/src/baekjoon_AO/PhoneBill.java
+++ Practice/src/baekjoon_AO/PhoneBill.java
@@ -1,10 +1,25 @@
 package baekjoon_AO;
 
+import java.util.Scanner;
+
 public class PhoneBill {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int sumY = 0, a;
+		int sumM = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int p = in.nextInt();
+			sumY += ((p / 30) + 1) * 10;
+			sumM += ((p / 60) + 1) * 15;
+		}
+		
+		if(sumY == sumM) System.out.println("Y M " + sumY);
+		else if(sumY > sumM) System.out.println("M " + sumM);
+		else System.out.println("Y " + sumY);
+		
 	}
 
 }
