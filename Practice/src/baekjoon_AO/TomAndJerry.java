d2ed49aa26838cb56a6715307b5d1932a13d83ce
jjwon915 <jjwon915@naver.com>
2021-06-06 23:38:11 +0900

    제리와 톰 - 백준 16430번 - 1Kg에서 제리가 A/Bkg 만큼 훔쳐간 상황이다. - 남은 치즈의 무게 구하기.

---
+++ Practice/src/baekjoon_AO/TomAndJerry.java
@@ -0,0 +1,16 @@
+package baekjoon_AO;
+
+import java.util.Scanner;
+
+public class TomAndJerry {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		System.out.println((b-a) + " " + b);
+
+	}
+
+}
