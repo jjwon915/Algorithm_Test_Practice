c6cca33c2943b28cd5f7421580c32ce6cb3e4246
jjwon915 <jjwon915@naver.com>
2021-06-07 02:01:01 +0900

    긴자리 계산 - 백준 2338번 - 1000자리 까지 숫자가 입력될 수 있기때문에 long형변수보다 큰 BigInteger를 사용했다. - BigInteger 클래스에서 제공하는 add, substract, multiply 메소드를 사용해 더하기, 빼기, 곱셈 연산을 구현하였다.

---
+++ Practice/src/baekjoon_AO/Calculate.java
@@ -0,0 +1,18 @@
+package baekjoon_AO;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BigInteger a = in.nextBigInteger();
+		BigInteger b = in.nextBigInteger();
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+		
+	}
+
+}
