b5d98853467e632430883b335c034b8d2eb78ac0
jjwon915 <jjwon915@naver.com>
2021-06-27 22:24:42 +0900

    피보나치 수 5 - 백준 10870번 - n번째 피보나치 수 구하기. - 함수를 만들어 구현하였다.

---
+++ Practice/src/baekjoon_math/Fibonacci_5.java
@@ -0,0 +1,23 @@
+package baekjoon_math;
+
+import java.util.Scanner;
+
+public class Fibonacci_5 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(fibonacci(n));
+
+	}
+	
+	public static int fibonacci(int n) {
+		if(n == 0)
+			return 0;
+		else if(n == 1)
+			return 1;
+		else
+			return fibonacci(n-1) + fibonacci(n-2);
+	}
+
+}
