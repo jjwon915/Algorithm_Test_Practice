3c0d132ab56a66f99b31861ca450f75a78f486b7
jjwon915 <jjwon915@naver.com>
2021-06-30 20:10:25 +0900

    숫자만 추출 - 문자열에서 숫자만 추출해 출력하기 - replaceAll()을 이용해 숫자가 아니면 문자를 제거하도록 구현했다. - 문자를 제거한 숫자만 남은 문자열을 Int형으로 변환하여 return 하였다.

---
+++ CodingInflearn/src/sectionOne/ExtractNumbers.java
@@ -0,0 +1,24 @@
+package sectionOne;
+
+import java.util.Scanner;
+
+public class ExtractNumbers {
+
+	public int solution(String str) {
+		
+		str = str.replaceAll("[^0-9]", "");
+		int answer = Integer.parseInt(str);
+		return answer;
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		ExtractNumbers T = new ExtractNumbers();
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		System.out.println(T.solution(str));
+		
+	}
+
+}
