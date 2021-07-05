82c5626ce3ee408439dacff07a81ae4b8c6d9f36
jjwon915 <jjwon915@naver.com>
2021-07-05 20:20:27 +0900

    큰 수 출력하기 - 빈칸으로 구분하여 String 배열로 변환하여 풀었다. - tmp에 arr[i] 를 저장해 arr[i+1]에 저장된 값과 비교하여 String answer에 더하는 방식으로 구현하였다.

---
+++ CodingInflearn/src/sectionOne/PrintLargeNumber.java
@@ -0,0 +1,40 @@
+package sectionOne;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class PrintLargeNumber {
+
+	public String solution(int n, String str) {
+		
+		String answer = "";
+		String[] arr = str.split(" ");
+		
+		int tmp = Integer.parseInt(arr[0]);
+		
+		for(int i = 0; i < n; i++) {
+			if(i == 0)
+				answer += tmp + " ";
+			else if(tmp < Integer.parseInt(arr[i]))
+				answer += Integer.parseInt(arr[i]) + " ";
+			
+			tmp = Integer.parseInt(arr[i]);
+			
+		}
+		
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		PrintLargeNumber T = new PrintLargeNumber();
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.nextLine();
+		String str = in.nextLine();
+		System.out.println(T.solution(n, str));
+
+	}
+
+}
