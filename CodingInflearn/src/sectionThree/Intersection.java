417658fe9fb0c047147f0f355fc6ab64cdaa677c
jjwon915 <jjwon915@naver.com>
2021-07-09 15:42:24 +0900

    공통원소 구하기 - 처음 배열에 저장한 뒤 오름차순 정렬하기, A, B 배열 모두 오름차순 정렬. - 그 다음 포인터 변수 p1, p2를 만들어 사용한다. - while(p1 < n && p2 < m)일때만 동작.

---
+++ CodingInflearn/src/sectionThree/Intersection.java
@@ -0,0 +1,62 @@
+package sectionThree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Intersection {
+	
+	public ArrayList<Integer> solution(int[] arr, int[] arr2, int n, int m){
+		
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		Arrays.sort(arr);
+		Arrays.sort(arr2);
+		
+		int p1 = 0, p2 = 0;
+		
+		while(p1 < n && p2 < m) {
+			int tmp1 = arr[p1];
+			int tmp2 = arr2[p2];
+			if(tmp1 == tmp2) {
+				answer.add(tmp1);
+				p1++;
+				p2++;
+			}
+			else if(tmp1 < tmp2) {
+				p1++;
+			}
+			else if(tmp2 < tmp1) {
+				p2++;
+			}
+			else {
+				p1++;
+				p2++;
+			}
+		}
+		
+		return answer;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Intersection T = new Intersection();
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		int m = in.nextInt();
+		int[] arr2 = new int[m];
+		for(int i = 0; i < m; i++) {
+			arr2[i] = in.nextInt();
+		}
+
+		for(int x : T.solution(arr, arr2, n, m)) {
+			System.out.print(x + " ");
+		}
+	}
+
+}
