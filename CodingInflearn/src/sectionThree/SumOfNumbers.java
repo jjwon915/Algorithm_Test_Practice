09b6fd4d9cc64a877c550e098b1e2aa2841a68d4
jjwon915 <jjwon915@naver.com>
2021-07-09 17:04:07 +0900

    연속된 자연수의 합 - 입력으로 양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는 방법의 가짓수 출력하기 - 앞의 문제와 풀이방법이 거의 동일하다. - 연속된 자연수의 합 이기 때문에 더해지는 최대 숫자가 n / 2 + 1이다. 그래서 for문을 rt = 1 부터 rt <= n/2 + 1일 때까지 동작하도록 구현하였다. - 순차적으로 증가하는 자연수를 sum에 더하기 때문에 배열을 따로 만들지 않고 lt와 rt값으로 구현하였다. - sum에 rt를 더해서 n과 비교하고, n보다 크면 while문을 통해 lt값을 빼는 방식으로 구현하였다.

---
+++ CodingInflearn/src/sectionThree/SumOfNumbers.java
@@ -0,0 +1,40 @@
+package sectionThree;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+	
+	public int solution(int n) {
+		
+		int answer = 0;
+		int sum = 0;
+		int lt = 0;
+		
+		if(n % 2 == 0)
+			return answer;
+		
+		for(int rt = 1; rt <= n / 2 + 1; rt++) {
+			sum = sum + rt;
+			if(sum == n) answer++;
+			while(sum > n) {
+				sum -= lt++;
+				if(sum == n) {
+					answer++;
+				}
+			}
+		}
+		
+		return answer;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		SumOfNumbers T = new SumOfNumbers();
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(T.solution(n));
+
+	}
+
+}
