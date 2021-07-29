83611463c489fd9b691c49ddd764bdd8690e01ae
jjwon915 <jjwon915@naver.com>
2021-07-29 22:25:40 +0900

    후위식 연산(postfix) - 후위연산식이 주어지면 연산한 결과를 출력하는 프로그램 작성하기. - String으로 연산식이 입력받고, solution 함수를 호출한다. - solution 함수에서 Stack을 생성한 다음 연산식을 toCharArray()를 이용해 하나씩 탐색한다. - 탐색한 원소가 숫자이면 Stack에 push하고 숫자가 아닌 연산자이면 Stack에서 두 수를 pop()해서 연산을 수행 후 다시 Stack에 연산 결과를 push()한다. - 단, 연산자가 -이거나 /인 경우에는 숫자의 순서가 중요하기 때문에 int tmp에 Stack의 원소를 pop()해서 저장한 후 연산을 수행한다.

---
+++ CodingInflearn/src/sectionFive/Postfix.java
@@ -0,0 +1,46 @@
+package sectionFive;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Postfix {
+
+	public int solution(String s) {
+		
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		
+		for(char x : s.toCharArray()) {
+			if(x >= '0' && x <= '9')
+				stack.push(x - '0');
+			else if(x == '+') {
+				stack.push(stack.pop() + stack.pop());
+			}
+			else if(x == '-') {
+				int tmp = -(stack.pop());
+				stack.push(stack.pop() + tmp);
+			}
+			else if(x == '*') {
+				stack.push(stack.pop() * stack.pop());
+			}
+			else if(x == '/') {
+				int tmp = stack.pop();
+				stack.push(stack.pop() / tmp);
+			}
+		}
+		answer = stack.pop();
+		
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Postfix T = new Postfix();
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		System.out.println(T.solution(s));
+
+	}
+
+}
